a06ca52a6d1fca6b32ff45279e14fd45af4a3e7c
Shigma <shigma10826@gmail.com>
2022-07-13 03:02:32 +0800

    chore: migrate from koishijs/koishi

---
+++ packages/client/client/components/chat/image.vue
@@ -0,0 +1,17 @@
+<template>
+  <img class="chat-image" :src="src" @click="handleClick"/>
+</template>
+
+<script lang="ts" setup>
+
+import { shared } from './utils'
+
+const props = defineProps<{ src: string }>()
+
+function handleClick(ev: MouseEvent) {
+  ev.preventDefault()
+  if (ev.metaKey) return window.open(props.src, '_blank')
+  shared.overlayImage = ev.target as HTMLImageElement
+}
+
+</script>
---
+++ packages/manager/client/settings/dep-link.vue
@@ -0,0 +1,28 @@
+<template>
+  <span class="link" @click.stop="configurate(name)">
+    {{ name }}
+  </span>
+</template>
+
+<script lang="ts" setup>
+
+import { store } from '@koishijs/client'
+import { addFavorite, gotoSettings } from '../utils'
+
+defineProps<{
+  name: string
+}>()
+
+function configurate(name: string) {
+  if (store.packages[name]) {
+    gotoSettings(name.replace('koishi-plugin-', '').replace('@koishijs/plugin-', ''))
+  } else {
+    addFavorite(name)
+  }
+}
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
