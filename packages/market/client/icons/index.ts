514f7a8b1b3a8216449d08612ffe5078b050121e
Shigma <shigma10826@gmail.com>
2022-08-17 03:30:18 +0800

    feat(market): support new plugin market

--- packages/market/client/icons/index.ts
+++ packages/market/client/icons/index.ts
@@ -4,6 +4,11 @@ import NavDeps from './activity/deps.vue'
 import NavMarket from './activity/market.vue'
 import NavPlugin from './activity/plugin.vue'
 
+import CategoryBusiness from './category/business.vue'
+import CategoryGame from './category/game.vue'
+import CategoryOther from './category/other.vue'
+import CategoryStorage from './category/storage.vue'
+
 import AddGroup from './settings/add-group.vue'
 import AddPlugin from './settings/add-plugin.vue'
 import TrashCan from './settings/trash-can.vue'
@@ -15,6 +20,11 @@ import Save from './settings/save.vue'
 icons.register('activity:deps', NavDeps)
 icons.register('activity:market', NavMarket)
 icons.register('activity:plugin', NavPlugin)
+
+icons.register('category:business', CategoryBusiness)
+icons.register('category:game', CategoryGame)
+icons.register('category:other', CategoryOther)
+icons.register('category:storage', CategoryStorage)
 
 icons.register('add-plugin', AddPlugin)
 icons.register('add-group', AddGroup)
--- packages/market/client/market/package.vue
+++ packages/market/client/market/package.vue
@@ -1,37 +1,44 @@
 <template>
-  <k-card class="market-view">
-    <template #header>
-      {{ data.shortname }}
-      <a v-if="data.links.homepage" :href="data.links.homepage" target="_blank" rel="noopener noreferrer">
-        <k-icon name="link"></k-icon>
-      </a>
-      <k-button v-if="!store.packages[data.name]" solid class="right" @click="$emit('click')">添加</k-button>
-      <k-button v-else type="success" solid class="right" @click="$emit('click')">修改</k-button>
-    </template>
-    <k-markdown inline class="desc" :source="meta.manifest.description.zh || meta.manifest.description.en"></k-markdown>
-    <div class="badges">
-      <k-badge type="success"
-        v-if="data.official"
-        @click="$emit('query', 'is:official')"
-      >官方</k-badge>
-      <k-badge type="primary"
-        v-if="meta.manifest.service.implements.includes('database')"
-        @click="$emit('query', 'impl:database')"
-      >数据库</k-badge>
-      <k-badge type="primary"
-        v-if="meta.manifest.service.implements.includes('adapter')"
-        @click="$emit('query', 'impl:adapter')"
-      >适配器</k-badge>
-      <k-badge type="primary"
-        v-if="meta.manifest.service.implements.includes('manifestassets')"
-        @click="$emit('query', 'impl:assets')"
-      >资源存储</k-badge>
-      <k-badge type="primary"
-        v-if="meta.manifest.service.required.includes('console') || meta.manifest.service.optional.includes('console')"
-        @click="$emit('query', 'using:console')"
-      >控制台</k-badge>
+  <section class="k-card market-view">
+    <div class="header">
+      <div class="left">
+        <k-icon :name="'category:' + resolveCategory(data.manifest.category)"></k-icon>
+      </div>
+      <div class="right">
+        <h2>
+          {{ data.shortname }}
+          <a v-if="data.links.homepage" :href="data.links.homepage" target="_blank" rel="noopener noreferrer">
+            <k-icon name="link"></k-icon>
+          </a>
+        </h2>
+        <div class="badges">
+          <k-badge type="success"
+            v-if="data.official"
+            @click="$emit('query', 'is:official')"
+          >官方</k-badge>
+          <k-badge type="primary"
+            v-if="meta.manifest.service.implements.includes('database')"
+            @click="$emit('query', 'impl:database')"
+          >数据库</k-badge>
+          <k-badge type="primary"
+            v-if="meta.manifest.service.implements.includes('adapter')"
+            @click="$emit('query', 'impl:adapter')"
+          >适配器</k-badge>
+          <k-badge type="primary"
+            v-if="meta.manifest.service.implements.includes('manifestassets')"
+            @click="$emit('query', 'impl:assets')"
+          >资源存储</k-badge>
+          <k-badge type="primary"
+            v-if="meta.manifest.service.required.includes('console') || meta.manifest.service.optional.includes('console')"
+            @click="$emit('query', 'using:console')"
+          >控制台</k-badge>
+        </div>
+        <k-button v-if="!store.packages[data.name]" solid class="right" @click="$emit('click')">添加</k-button>
+        <k-button v-else type="success" solid class="right" @click="$emit('click')">修改</k-button>
+      </div>
     </div>
-    <template #footer>
+    <k-markdown inline class="desc" :source="meta.manifest.description.zh || meta.manifest.description.en"></k-markdown>
+    <div class="footer">
       <div class="info">
         <span><k-icon name="tag"></k-icon>{{ data.version }}</span>
         <span v-if="data.installSize"><k-icon name="file-archive"></k-icon>{{ formatSize(data.installSize) }}</span>
@@ -44,8 +51,8 @@
           </a>
         </el-tooltip>
       </div>
-    </template>
-  </k-card>
+    </div>
+  </section>
 </template>
 
 <script lang="ts" setup>
@@ -62,6 +69,13 @@ const props = defineProps({
 })
 
 const meta = computed(() => getMixedMeta(props.data.name))
+
+const categories = ['game', 'business']
+
+function resolveCategory(name: string) {
+  if (categories.includes(name)) return name
+  return 'other'
+}
 
 function formatValue(value: number) {
   return value >= 100 ? +value.toFixed() : +value.toFixed(1)
@@ -84,40 +98,81 @@ function formatSize(value: number) {
 .market-view {
   width: 100%;
   max-width: 540px;
-  height: 192px;
+  height: 13rem;
   margin: 0;
+  padding: 1.25rem;
+  box-sizing: border-box;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  gap: 1rem;
 
-  header, footer {
-    margin: 1rem 0;
-    flex-shrink: 0;
+  .header, .footer {
+    flex: 0 0 auto;
   }
 
-  .k-card-body {
-    margin: -1rem 0;
-    flex-grow: 1;
-    overflow: hidden;
+  .header {
+    position: relative;
     display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
+    gap: 1rem;
+
+    .left {
+      width: 4rem;
+      height: 4rem;
+      border-radius: 8px;
+      border: 1px solid var(--card-border);
+      box-sizing: border-box;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      svg {
+        height: 1.75rem;
+      }
+    }
+
+    .right {
+      display: flex;
+      flex-flow: column;
+      justify-content: space-around;
+
+      .badges {
+        height: 1.5rem;
+      }
+    }
+
+    h2 {
+      font-size: 1.125rem;
+      margin: 0;
+      line-height: 1;
+    }
+
+    .right .k-icon {
+      color: var(--fg1);
+      margin-left: 0.6rem;
+      height: 0.875rem;
+      transition: color 0.3s ease;
+    }
+
+    button.right {
+      position: absolute;
+      right: 0;
+      top: -3px;
+      padding: 0.35em 0.85em;
+    }
   }
 
   .desc {
     margin: 0;
     font-size: 15px;
+    flex: 1 1 auto;
   }
 
-  header {
-    font-size: 1.125rem;
-  }
-
-  footer {
+  .footer {
     display: flex;
     align-items: center;
     justify-content: space-between;
     height: 1.5rem;
+    margin-bottom: -0.25rem;
 
     .avatars a {
       cursor: pointer;
@@ -130,21 +185,6 @@ function formatSize(value: number) {
       border-radius: 100%;
       vertical-align: middle;
     }
-  }
-
-  header .k-icon {
-    color: var(--fg1);
-    margin-left: 0.6rem;
-    height: 1rem;
-    vertical-align: -1px;
-    transition: color 0.3s ease;
-  }
-
-  button.right {
-    position: absolute;
-    right: 1rem;
-    top: -3px;
-    padding: 0.35em 0.85em;
   }
 
   .info {
