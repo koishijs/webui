7a7fc9cf073ddf38b2cf6eaa2a21a5235ebfdbc4
42 <37006258+MirrorCY@users.noreply.github.com>
2022-11-13 12:59:22 +0000

    add noop()
    Signed-off-by: 42 <37006258+MirrorCY@users.noreply.github.com>

--- packages/sandbox/src/index.ts
+++ packages/sandbox/src/index.ts
@@ -1,4 +1,4 @@
-import { Bot, Context, defineProperty, Dict, Fragment, observe, Random, Schema, segment, User } from 'koishi'
+import { Bot, Context, defineProperty, Dict, Fragment, observe, Random, Schema, segment, User, noop } from 'koishi'
 import { DataService, SocketHandle } from '@koishijs/plugin-console'
 import { resolve } from 'path'
 import zh from './locales/zh.yml'
@@ -98,7 +98,7 @@ class SandboxBot extends Bot {
     return ids
   }
 
-  async deleteMessage(){} //empty function, fix https://github.com/koishijs/novelai-bot/issues/114
+  async deleteMessage(){noop()} //empty function, fix https://github.com/koishijs/novelai-bot/issues/114
 
   async getGuildMemberList(guildId: string) {
     return words.map((word) => ({
