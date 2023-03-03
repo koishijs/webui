158b2419ccbbdb054a9527614895794e6dd07763
Shigma <shigma10826@gmail.com>
2023-03-04 00:06:03 +0800

    fix(market): fix an edge case for underscore service

--- packages/market/src/shared/service.ts
+++ packages/market/src/shared/service.ts
@@ -1,20 +1,23 @@
 import { DataService } from '@koishijs/plugin-console'
 import { Context, Dict } from 'koishi'
 
-export class ServiceProvider extends DataService<Dict<string>> {
+export class ServiceProvider extends DataService<Dict<number>> {
   constructor(ctx: Context) {
     super(ctx, 'services')
     ctx.on('internal/service', () => this.refresh())
   }
 
   async get() {
-    const services = {} as Dict<string>
+    const services = {} as Dict<number>
     const descriptors = Object.getOwnPropertyDescriptors(Context.prototype)
     for (const key in descriptors) {
       const desc = descriptors[key]
       if ('value' in desc) continue
-      const uid = this.ctx[key]?.[Context.source]?.state.uid
-      if (uid) services[key] = uid
+      const ctx: Context = this.ctx[key]?.[Context.source]
+      if (ctx?.scope.uid) {
+        const name = key.replace(/^__/, '').replace(/__$/, '')
+        services[name] = ctx.scope.uid
+      }
     }
     return services
   }
