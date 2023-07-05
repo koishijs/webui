26bd09698d198d25f6469b85273ed47ed35b87c1
Shigma <shigma10826@gmail.com>
2023-07-06 00:18:33 +0800

    online: enhance workspace plugin loading

--- packages/online/scripts/dev.ts
+++ packages/online/scripts/dev.ts
@@ -1,9 +1,12 @@
 import { createReadStream, Stats } from 'fs'
 import { readFile, stat } from 'fs/promises'
-import { extname, resolve } from 'path'
+import { dirname, extname, resolve } from 'path'
 import { ViteDevServer } from 'vite'
 import { LocalScanner } from '@koishijs/registry'
 import { noop } from '@koishijs/utils'
+import { load } from 'tsconfig-utils'
+import { createRequire } from 'module'
+import { PackageJson } from 'yakumo'
 import yaml from '@maikolib/vite-plugin-yaml'
 import Koa from 'koa'
 import Router from '@koa/router'
@@ -45,18 +48,6 @@ router.get(uiPath + '(/.+)*', async (ctx, next) => {
   ctx.body = await transformHtml(template)
 })
 
-const browserEntries = {
-  '@koishijs/plugin-config': '../src/browser/index.ts',
-  '@koishijs/plugin-console': '../src/browser/index.ts',
-  '@koishijs/plugin-market': '../src/browser/index.ts',
-}
-
-router.get('(/.+)*/sql-wasm.wasm', async (ctx) => {
-  const filename = require.resolve('@minatojs/sql.js/dist/sql-wasm.wasm')
-  ctx.body = createReadStream(filename)
-  ctx.type = 'application/wasm'
-})
-
 const scanner = new LocalScanner(__dirname)
 
 router.get('/portable.json', async (ctx) => {
@@ -69,9 +60,69 @@ router.get('/portable.json', async (ctx) => {
   ctx.body = scanner
 })
 
-router.get('/modules(/.+)+/index.js', async (ctx) => {
+function getConditionalExport(exports: PackageJson.Exports) {
+  if (typeof exports === 'string') return exports
+  for (const key of ['browser', 'import', 'default', '.']) {
+    if (exports[key]) return getConditionalExport(exports[key])
+  }
+}
+
+function getExport(meta: PackageJson) {
+  if (meta.exports) {
+    return getConditionalExport(meta.exports)
+  } else if (typeof meta.module === 'string') {
+    return meta.module
+  } else if (typeof meta.main === 'string') {
+    return meta.main
+  } else {
+    return 'index.js'
+  }
+}
+
+router.get('/modules(/.+)+', async (ctx) => {
+  const parts = ctx.params[0].slice(1).split('/')
+  let name = parts.shift()
+  if (name.startsWith('@')) name += '/' + parts.shift()
+  const filename = parts.join('/')
+  const metafile = require.resolve(name + '/package.json')
+  const cwd = resolve(metafile, '..')
+  const meta = await readFile(metafile, 'utf8').then(JSON.parse) as PackageJson
+  if (filename === 'index.js') {
+    const config = await load(cwd)
+    const { rootDir, outFile, outDir = dirname(outFile) } = config.compilerOptions
+    let entry = getExport(meta)
+    if (entry.startsWith('./')) entry = entry.slice(2)
+    if (entry.startsWith(outDir + '/')) {
+      const outExt = extname(entry)
+      entry = resolve(cwd, rootDir, entry.slice(outDir.length + 1, -outExt.length) + '.ts')
+    } else {
+      entry = resolve(cwd, entry)
+    }
+    ctx.redirect(`/vite/@fs${entry}`)
+  } else {
+    let entry = (meta['koishi'].exports || {})[filename]
+    const require = createRequire(metafile)
+    entry = require.resolve(entry)
+    ctx.body = createReadStream(entry)
+    ctx.type = 'application/wasm'
+  }
+})
+
+router.get('/modules(/.+)+', async (ctx) => {
   const name = ctx.params[0].slice(1)
-  const entry = resolve(require.resolve(name + '/package.json'), browserEntries[name] || '../src/index.ts')
+  const metapath = require.resolve(name + '/package.json')
+  const cwd = resolve(metapath, '..')
+  const meta = await readFile(metapath, 'utf8').then(JSON.parse) as PackageJson
+  const config = await load(cwd)
+  const { rootDir, outFile, outDir = dirname(outFile) } = config.compilerOptions
+  let entry = getExport(meta)
+  if (entry.startsWith('./')) entry = entry.slice(2)
+  if (entry.startsWith(outDir + '/')) {
+    const outExt = extname(entry)
+    entry = resolve(cwd, rootDir, entry.slice(outDir.length + 1, -outExt.length) + '.ts')
+  } else {
+    entry = resolve(cwd, entry)
+  }
   ctx.redirect(`/vite/@fs${entry}`)
 })
 
@@ -108,11 +159,16 @@ async function createVite() {
       extensions: ['.ts', '.js', '.json', '.yml', '.yaml'],
       dedupe: ['vue', 'vue-demi', 'vue-router', 'element-plus', '@vueuse/core', '@popperjs/core'],
       alias: {
-        '@koishijs/core': '@koishijs/core/src/index.ts',
-        '@koishijs/loader': '@koishijs/loader/src/shared.ts',
+        ...require.resolve('@root/koishi/package.json') ? {
+          '@koishijs/core': '@koishijs/core/src/index.ts',
+          '@koishijs/loader': '@koishijs/loader/src/shared.ts',
+        } : {},
+        ...require.resolve('@root/minato/package.json') ? {
+          '@minatojs/driver-sqlite': '@minatojs/driver-sqlite/src/index.ts',
+          '@minatojs/sql-utils': '@minatojs/sql-utils/src/index.ts',
+          '@minatojs/core': '@minatojs/core/src/index.ts',
+        } : {},
         '@koishijs/plugin-console': '@koishijs/plugin-console/src/browser/index.ts',
-        '@minatojs/driver-sqlite': '@minatojs/driver-sqlite/src/index.ts',
-        '@minatojs/sql-utils': '@minatojs/sql-utils/src/index.ts',
         'chokidar': '@koishijs/fs/src/index.ts',
         'fs/constants': '@koishijs/fs/src/constants.ts',
         'fs/promises': '@koishijs/fs/src/promises.ts',
@@ -133,7 +189,6 @@ async function createVite() {
         'schemastery',
         'element-plus',
         'supports-color',
-        'rollup-plugin-node-polyfills/polyfills/path',
         'marked',
         'xss',
         'semver',
