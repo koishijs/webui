37853d7be90753293b68133db43f17f6aa04df8d
Shigma <shigma10826@gmail.com>
2023-07-10 01:45:17 +0800

    feat(config): support config schema reload

--- plugins/config/src/browser/packages.ts
+++ plugins/config/src/browser/packages.ts
@@ -1,11 +1,6 @@
 import * as shared from '../shared'
 
 export class PackageProvider extends shared.PackageProvider {
-  import(name: string) {
-    const shortname = name.replace(/(koishi-|^@koishijs\/)plugin-/, '')
-    return this.ctx.loader.resolvePlugin(shortname)
-  }
-
   async collect(forced: boolean) {
     return this.ctx.loader.market.objects
   }
