d3d25b23a4105c942372075d08c9961e1a4e4f30
Shigma <shigma10826@gmail.com>
2023-09-10 14:54:37 +0800

    feat(client): add insert above/below button for computed

--- packages/components/client/form/computed.vue
+++ packages/components/client/form/computed.vue
@@ -15,14 +15,14 @@
     </template>
     <template #menu>
       <div class="k-menu-separator"></div>
-      <div class="k-menu-item" @click="actions.add">
+      <div class="k-menu-item" @click="actions.insert()">
         <span class="k-menu-icon"><icon-branch></icon-branch></span>
         添加分支
       </div>
     </template>
     <template #prefix><slot name="prefix"></slot></template>
     <template #suffix>
-      <el-button v-if="isSwitch" @click="actions.add">添加分支</el-button>
+      <el-button v-if="isSwitch" @click="actions.insert()">添加分支</el-button>
     </template>
     <template #collapse v-if="isSwitch">
       <k-schema
@@ -46,6 +46,14 @@
           <div class="k-menu-item" :class="{ disabled }" @click="actions.delete(index)">
             <span class="k-menu-icon"><icon-delete></icon-delete></span>
             删除分支
+          </div>
+          <div class="k-menu-item" :class="{ disabled }" @click="actions.insert(index)">
+            <span class="k-menu-icon"><icon-insert-before></icon-insert-before></span>
+            在上方插入分支
+          </div>
+          <div class="k-menu-item" :class="{ disabled }" @click="actions.insert(index + 1)">
+            <span class="k-menu-icon"><icon-insert-after></icon-insert-after></span>
+            在下方插入分支
           </div>
         </template>
         <template #title>
@@ -119,10 +127,10 @@ const actions = {
     branches[index] = { ...branches[index], [key]: value }
     emit('update:modelValue', { $switch: { ...props.modelValue.$switch, branches } })
   },
-  add() {
+  insert(index: number = props.modelValue?.$switch.branches.length) {
     if (props.modelValue?.$switch) {
       const branches = props.modelValue.$switch.branches.slice()
-      branches.push({ case: null, then: null })
+      branches.splice(index, 0, { case: null, then: null })
       emit('update:modelValue', { $switch: { ...props.modelValue.$switch, branches } })
     } else {
       emit('update:modelValue', {
