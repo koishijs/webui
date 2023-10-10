eee59bb65a951f130ac80aaaf0f9adcf31833b45
Shigma <shigma10826@gmail.com>
2023-10-11 02:44:55 +0800

    feat(locales): support multiple roots

--- plugins/locales/src/index.ts
+++ plugins/locales/src/index.ts
@@ -1,10 +1,9 @@
 import { Context, Dict, I18n, Logger, Schema } from 'koishi'
 import { DataService } from '@koishijs/console'
-import { cp, mkdir, readdir, readFile, rm, stat, writeFile } from 'fs/promises'
+import { mkdir, readdir, readFile, writeFile } from 'fs/promises'
 import { resolve } from 'path'
 import { debounce } from 'throttle-debounce'
 import { dump, load } from 'js-yaml'
-import { Stats } from 'fs'
 
 declare module '@koishijs/console' {
   namespace Console {
@@ -43,7 +42,7 @@ class LocaleProvider extends DataService<Dict<I18n.Store>> {
       for (const locale in data) {
         ctx.i18n.define('$' + locale, data[locale])
         const content = dump(data[locale])
-        await writeFile(resolve(ctx.baseDir, config.root, locale + '.yml'), content)
+        await writeFile(resolve(ctx.baseDir, config.root[0], locale + '.yml'), content)
       }
     })
   }
@@ -56,32 +55,30 @@ class LocaleProvider extends DataService<Dict<I18n.Store>> {
 export const name = 'locales'
 
 export interface Config {
-  root?: string
+  root?: string[]
 }
 
 export const Config: Schema<Config> = Schema.object({
-  root: Schema.path({
-    filters: ['directory'],
-    allowCreate: true,
-  }).default('data/locales').description('存放本地化文件的根目录。'),
+  root: Schema.union([
+    Schema.array(Schema.path({
+      filters: ['directory'],
+      allowCreate: true,
+    })),
+    Schema.transform(String, (root) => [root]),
+  ]).default(['data/locales', 'locales']).description('存放本地化文件的根目录。'),
 })
 
 export async function apply(ctx: Context, config: Config) {
-  const legacy = resolve(ctx.baseDir, 'locales')
-  const folder = resolve(ctx.baseDir, config.root)
-  await mkdir(folder, { recursive: true })
-  const stats: Stats = await stat(legacy).catch(() => null)
-  if (stats?.isDirectory()) {
-    logger.info('migrating to data directory')
-    await cp(legacy, folder)
-    await rm(legacy, { recursive: true, force: true })
-  }
-  const files = await readdir(folder)
-  for (const file of files) {
-    if (!file.endsWith('.yml')) continue
-    logger.debug('loading locale %s', file)
-    const content = await readFile(resolve(folder, file), 'utf8')
-    ctx.i18n.define('$' + file.split('.')[0], load(content) as any)
+  for (const root of config.root.slice().reverse()) {
+    const folder = resolve(ctx.baseDir, root)
+    await mkdir(folder, { recursive: true })
+    const files = await readdir(folder)
+    for (const file of files) {
+      if (!file.endsWith('.yml')) continue
+      logger.debug('loading locale %s', file)
+      const content = await readFile(resolve(folder, file), 'utf8')
+      ctx.i18n.define('$' + file.split('.')[0], load(content) as any)
+    }
   }
 
   ctx.plugin(LocaleProvider, config)
