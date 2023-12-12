00325391e134acf99c1e46039f49d7a0c5f929bc
Shigma <shigma10826@gmail.com>
2023-12-13 01:27:59 +0800

    chore: migrate to yakumo v1 and yarn v4

--- packages/client/src/yakumo.ts
+++ packages/client/src/yakumo.ts
@@ -1,6 +1,6 @@
 import { createRequire } from 'module'
 import { UserConfig } from 'vite'
-import { register } from 'yakumo'
+import { Context } from 'yakumo'
 import { buildExtension } from '.'
 import ns from 'ns-require'
 
@@ -10,27 +10,29 @@ declare module 'yakumo' {
   }
 }
 
-register('client', async (project) => {
-  for (const path in project.targets) {
-    const meta = project.targets[path]
-    const deps = {
-      ...meta.dependencies,
-      ...meta.devDependencies,
-      ...meta.peerDependencies,
-      ...meta.optionalDependencies,
-    }
-    let config: UserConfig = {}
-    if (meta.yakumo?.client) {
-      const require = createRequire(project.cwd + path + '/package.json')
-      const exports = ns.unwrapExports(require(meta.yakumo.client))
-      if (typeof exports === 'function') {
-        await exports()
+export function apply(ctx: Context) {
+  ctx.register('client', async () => {
+    for (const path in ctx.yakumo.targets) {
+      const meta = ctx.yakumo.targets[path]
+      const deps = {
+        ...meta.dependencies,
+        ...meta.devDependencies,
+        ...meta.peerDependencies,
+        ...meta.optionalDependencies,
+      }
+      let config: UserConfig = {}
+      if (meta.yakumo?.client) {
+        const require = createRequire(ctx.yakumo.cwd + path + '/package.json')
+        const exports = ns.unwrapExports(require(meta.yakumo.client))
+        if (typeof exports === 'function') {
+          await exports()
+          continue
+        }
+        config = exports
+      } else if (!deps['@koishijs/client']) {
         continue
       }
-      config = exports
-    } else if (!deps['@koishijs/client']) {
-      continue
+      await buildExtension(ctx.yakumo.cwd + path, config)
     }
-    await buildExtension(project.cwd + path, config)
-  }
-})
+  })
+}
