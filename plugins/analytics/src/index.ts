70a62428ed34552f547a1967463279bf451b79fe
Shigma <shigma10826@gmail.com>
2023-12-15 16:16:37 +0800

    feat(analytics): new audit tables

--- plugins/analytics/src/index.ts
+++ plugins/analytics/src/index.ts
@@ -1,52 +1,308 @@
-import { Context, Schema } from 'koishi'
+import { $, Context, deepEqual, Dict, Logger, Row, Schema, Session, Time } from 'koishi'
+import { DataService } from '@koishijs/console'
 import { resolve } from 'path'
-import {} from '@koishijs/console'
-import MetaProvider from './meta'
-import Analytics from './stats'
-
-export type Activity = Record<number, number>
 
 declare module 'koishi' {
-  interface Channel {
-    name: string
-    activity: Activity
+  interface Tables {
+    'analytics.message': Analytics.Message
+    'analytics.command': Analytics.Command
   }
 }
 
 declare module '@koishijs/console' {
   namespace Console {
     interface Services {
-      meta: MetaProvider
       analytics: Analytics
     }
   }
 }
 
-export {
-  MetaProvider,
-  Analytics as StatisticsProvider,
+const logger = new Logger('analytics')
+
+class Analytics extends DataService<Analytics.Payload> {
+  static inject = ['database', 'console']
+
+  lastUpdate = new Date()
+  updateHour = this.lastUpdate.getHours()
+  cachedDate: number
+  cachedData: Promise<Analytics.Payload>
+
+  private messages: Analytics.Message[] = []
+  private commands: Analytics.Command[] = []
+
+  constructor(ctx: Context, private config: Analytics.Config = {}) {
+    super(ctx, 'analytics')
+
+    ctx.model.extend('analytics.message', {
+      date: 'integer',
+      hour: 'integer',
+      type: 'string',
+      selfId: 'string',
+      platform: 'string',
+      count: 'integer',
+    }, {
+      primary: ['date', 'hour', 'type', 'selfId', 'platform'],
+    })
+
+    ctx.model.extend('analytics.command', {
+      date: 'integer',
+      hour: 'integer',
+      name: 'string',
+      selfId: 'string',
+      userId: 'string',
+      channelId: 'string',
+      platform: 'string',
+      count: 'integer',
+    }, {
+      primary: ['date', 'hour', 'name', 'selfId', 'userId', 'channelId', 'platform'],
+    })
+
+    ctx.on('exit', () => this.upload(true))
+
+    ctx.on('dispose', async () => {
+      await this.upload(true)
+    })
+
+    ctx.on('message', (session) => {
+      if (session.bot.hidden) return
+      this.addAudit(this.messages, {
+        ...this.createIndex(session),
+        type: 'receive',
+      })
+      this.upload()
+    })
+
+    ctx.on('send', (session) => {
+      if (session.bot.hidden) return
+      this.addAudit(this.messages, {
+        ...this.createIndex(session),
+        type: 'send',
+      })
+      this.upload()
+    })
+
+    ctx.any().before('command/execute', ({ command, session }) => {
+      this.addAudit(this.commands, {
+        ...this.createIndex(session),
+        name: command.name,
+        userId: session.user['id'],
+        channelId: session.channelId,
+      })
+      this.upload()
+    })
+
+    ctx.console.addEntry({
+      dev: resolve(__dirname, '../client/index.ts'),
+      prod: resolve(__dirname, '../dist'),
+    })
+  }
+
+  private createIndex(session: Session): Analytics.Index {
+    return {
+      selfId: session.selfId,
+      platform: session.platform,
+      date: Time.getDateNumber(),
+      hour: new Date().getHours(),
+    }
+  }
+
+  private addAudit<T extends Analytics.Message | Analytics.Command>(buffer: T[], index: Omit<T, 'count'>) {
+    const audit = buffer.find(data => deepEqual(data, index))
+    if (audit) {
+      audit.count += 1
+    } else {
+      buffer.push({ ...index, count: 1 } as T)
+    }
+  }
+
+  private async uploadAudit(table: string, buffer: Analytics.Audit[]) {
+    if (!buffer.length) return
+    await this.ctx.database.upsert(table as any, (row: Row<Analytics.Audit>) => buffer.map((audit) => ({
+      ...audit,
+      count: $.add($.ifNull(row.count, 0), audit.count),
+    })))
+    buffer.splice(0)
+  }
+
+  async upload(forced = false) {
+    const date = new Date()
+    const dateHour = date.getHours()
+    if (forced || +date - +this.lastUpdate > this.config.statsInternal || dateHour !== this.updateHour) {
+      this.lastUpdate = date
+      this.updateHour = dateHour
+      await Promise.all([
+        this.uploadAudit('analytics.message', this.messages),
+        this.uploadAudit('analytics.command', this.commands),
+      ])
+      logger.debug('analytics updated')
+    }
+  }
+
+  private async getCommandRate() {
+    const data = await this.ctx.database
+      .select('analytics.command', {
+        date: { $gt: Time.getDateNumber() - 7 },
+      })
+      .groupBy(['name'], {
+        count: row => $.sum(row.count),
+      })
+      .execute()
+    const result = {} as Dict<number>
+    data.forEach((stat) => {
+      result[stat.name] = stat.count / 7
+    })
+    return result
+  }
+
+  private async getDauHistory() {
+    const data = await this.ctx.database
+      .select('analytics.command')
+      .groupBy(['date'], {
+        count: row => $.count(row.userId),
+      })
+      .execute()
+    const result = {} as Dict<number>
+    data.forEach((stat) => {
+      result[Time.fromDateNumber(stat.date).toLocaleDateString('zh-CN')] = stat.count
+    })
+    return result
+  }
+
+  private async getMessageByBot() {
+    const data = await this.ctx.database
+      .select('analytics.message', {
+        date: { $gt: Time.getDateNumber() - 7 },
+      })
+      .groupBy(['type', 'platform', 'selfId'], {
+        count: row => $.sum(row.count),
+      })
+      .execute()
+    const result = {} as Dict<Dict<Dict<number, 'send' | 'receive'>>>
+    data.forEach((stat) => {
+      const entry = (result[stat.platform] ||= {})[stat.selfId] ||= { send: 0, receive: 0 }
+      entry[stat.type] = stat.count / 7
+    })
+    return result
+  }
+
+  private async getMessageByDate() {
+    const data = await this.ctx.database
+      .select('analytics.message')
+      .groupBy(['type', 'date'], {
+        count: row => $.sum(row.count),
+      })
+      .execute()
+    const result = {} as Dict<Dict<number, 'send' | 'receive'>>
+    data.forEach((stat) => {
+      const entry = result[Time.fromDateNumber(stat.date).toLocaleDateString('zh-CN')] ||= { send: 0, receive: 0 }
+      entry[stat.type] = stat.count
+    })
+    return result
+  }
+
+  private async getMessageByHour() {
+    const data = await this.ctx.database
+      .select('analytics.message', {
+        date: { $gt: Time.getDateNumber() - 7 },
+      })
+      .groupBy(['type', 'hour'], {
+        count: row => $.sum(row.count),
+      })
+      .execute()
+    const result = new Array(24).fill(null).map(() => ({ send: 0, receive: 0 }))
+    data.forEach((stat) => {
+      result[stat.hour][stat.type] = stat.count / 7
+    })
+    return result
+  }
+
+  async download(): Promise<Analytics.Payload> {
+    const [
+      userCount,
+      userIncrement,
+      commandRate,
+      dauHistory,
+      messageByBot,
+      messageByDate,
+      messageByHour,
+    ] = await Promise.all([
+      this.ctx.database.eval('user', row => $.count(row.id)),
+      this.ctx.database.eval('user', row => $.count(row.id), {
+        createdAt: {
+          $gt: Time.fromDateNumber(Time.getDateNumber() - 1),
+          $lt: Time.fromDateNumber(Time.getDateNumber()),
+        },
+      }),
+      this.getCommandRate(),
+      this.getDauHistory(),
+      this.getMessageByBot(),
+      this.getMessageByDate(),
+      this.getMessageByHour(),
+    ])
+    return {
+      userCount,
+      userIncrement,
+      commandRate,
+      dauHistory,
+      messageByBot,
+      messageByDate,
+      messageByHour,
+    }
+  }
+
+  async get() {
+    const date = new Date()
+    const dateNumber = Time.getDateNumber(date, date.getTimezoneOffset())
+    if (dateNumber !== this.cachedDate) {
+      this.cachedData = this.download()
+      this.cachedDate = dateNumber
+    }
+    return this.cachedData
+  }
 }
 
-export * from './meta'
-export * from './stats'
+namespace Analytics {
+  export interface Index {
+    id?: number
+    date: number
+    hour: number
+    selfId: string
+    platform: string
+  }
 
-export const filter = false
-export const name = 'status'
-export const inject = ['console']
+  export interface Audit extends Index {
+    count: number
+  }
 
-export interface Config extends MetaProvider.Config, Analytics.Config {}
+  export interface Message extends Index {
+    type: string
+    count: number
+  }
 
-export const Config: Schema<Config> = Schema.intersect([
-  MetaProvider.Config,
-  Analytics.Config,
-])
+  export interface Command extends Index {
+    name: string
+    userId: string
+    channelId: string
+    count: number
+  }
 
-export function apply(ctx: Context, config: Config) {
-  ctx.console.addEntry({
-    dev: resolve(__dirname, '../client/index.ts'),
-    prod: resolve(__dirname, '../dist'),
+  export interface Payload {
+    userCount: number
+    userIncrement: number
+    dauHistory: Dict<number>
+    commandRate: Dict<number>
+    messageByBot: Dict<Dict<Dict<number, 'send' | 'receive'>>>
+    messageByDate: Dict<Dict<number, 'send' | 'receive'>>
+    messageByHour: Dict<number, 'send' | 'receive'>[]
+  }
+
+  export interface Config {
+    statsInternal?: number
+  }
+
+  export const Config: Schema<Config> = Schema.object({
+    statsInternal: Schema.natural().role('ms').description('统计数据推送的时间间隔。').default(Time.minute * 10),
   })
-
-  ctx.plugin(MetaProvider, config)
-  ctx.plugin(Analytics, config)
 }
+
+export default Analytics
